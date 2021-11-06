b1df15cb888cd9d28411c17d65dfc34d78ebe3b0
Tyler Hawkins <udlr121@gmail.com>
2021-11-06 02:14:44 -0400

    Organize functions and prepare testing

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-function add(a, b) {
-	return a + b;
-}
+const operators = { 
+	add: (a, b) => {
+		return a + b;
+	},
 
-function subtract(a, b) {
-	return a - b;
-}
+	subtract: (a, b) => {
+		return a - b;
+	},
 
-function multiply(a, b) {
-	return a * b;
-}
+	multiply: (a, b) => {
+		return a * b;
+	},
 
-function divide(a, b) {
-	if (b == 0) {
-		return "Error";
-	} else {
-		return a / b;
+	divide: (a, b) => {
+		if (b == 0) {
+			return "Error";
+		} else {
+			return a / b;
+		}
 	}
 }
 
 function operate(operator, a, b) {
-	switch(operator) {
-		case "add":
-			return add(a, b);
-		case "subtract":
-			return subtract(a, b);
-		case "multiply":
-			return multiply(a, b);
-		case "divide":
-			return divide(a, b);
-	}
+	console.log(operator, a, b);
+	const result = operators[operator](a, b);
+	display(result);
+}
+
+function display(value) {
+	
 }
